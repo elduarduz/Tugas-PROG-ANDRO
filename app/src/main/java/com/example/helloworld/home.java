fc79fca1e94ced5c8c4fdfc376e92a8abbf0884a
elduarduz <54306639+elduarduz@users.noreply.github.com>
2020-03-06 11:58:52 +0700

    hello

--- app/src/main/java/com/example/helloworld/home.java
+++ app/src/main/java/com/example/helloworld/home.java
@@ -2,7 +2,16 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.app.FragmentManager;
+
+
 
 public class home extends AppCompatActivity {
 
@@ -10,5 +19,31 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void ChangeFragment(View view){
+        Fragment fragment;
+
+        if(view == findViewById(R.id.button)){
+            fragment = new PROFILE();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+        else if(view ==  findViewById(R.id.button2)){
+            fragment = new CHAT();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+        else if(view == findViewById(R.id.button3)){
+            fragment = new SETTINGS();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
     }
 }
